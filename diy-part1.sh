e1133e2776026f34555b753e9664f4a22a040ae4
fonlan <fonlan@gmail.com>
2021-05-26 17:19:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,10 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git fonlan https://github.com/fonlan/openwrt-package' feeds.conf.default
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.123.10/g' package/base-files/files/bin/config_generate
